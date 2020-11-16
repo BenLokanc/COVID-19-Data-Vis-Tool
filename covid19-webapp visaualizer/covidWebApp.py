b4a8791e969d1b628bf173ae33bf8b305d2e526c
BenLowkey <ben.lokanc@gmail.com>
2020-11-15 19:48:21 -0700

    Cleaning of files and addition of features
    Added titles to the graphs. cleaned up the code.

--- covid19-webapp visaualizer/covidWebApp.py
+++ covid19-webapp visaualizer/covidWebApp.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template
-
 from sqlalchemy import create_engine 
 from sqlalchemy import inspect
-
 import csv
 from collections import Counter
-
 import matplotlib.pyplot as plt 
 from matplotlib.ticker import FormatStrFormatter
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-
 import pandas as pd
 import seaborn as sns
 import datetime
@@ -24,19 +20,20 @@ with open ('covid19dataexport.csv') as covid:
         datelist.append(line['Date reported']) #datelist will be filled with just the dates in the csv file 
     datecountlist = sorted(Counter(datelist).items()) #list of tuples sorted by date. Date is paired with total cases 
     datelist,caselist = map(list,zip(*datecountlist)) #unpacking the list of tuples into 2 respective lists 
-        
-df = pd.DataFrame({'date': datelist, 'daily_cases': caselist}) #creates dataframe for dates and daily_cases
+
+#Creating dataframe of useful data from covid19dataexport.csv 
+df = pd.DataFrame({'date': datelist, 'daily_cases': caselist}) 
+
+#Creating sql database of values from dataframe
 db_uri = 'sqlite:///data/covidData'
 engine = create_engine(db_uri, echo=False)
 df.to_sql(name='alberta', con=engine, if_exists='replace')
 engine.dispose()
 
+#Creating the web-app
 app = Flask(__name__)
-
-
 @app.route('/')
 def plot_covid():
-
     try:
         name = 'alberta'
         engine = create_engine(db_uri, echo = False)
@@ -44,30 +41,26 @@ def plot_covid():
         print(df.head())
         engine.dispose()
 
+        #First figure, subplots of total cases and daily cases 
         fig1, (ax1,ax2) = plt.subplots(nrows=2,ncols=1,sharex=True)
+        fig1.suptitle("Plots of total cases and daily cases from March 6th to May 14th", horizontalalignment='center', fontsize= 12)
         FigureCanvas(fig1)
-
         #Make sure dates are sorted
         df = df.sort_values(by=['date'])
-
         #Compute total and smoothed daily cases
         df['total_cases'] = df.daily_cases.cumsum()
         df['smooth_daily_cases'] = df.daily_cases.rolling(3).mean()
-
         #Checking what we have
         df.info()
         print(df.head())
-
         #Figure1: Seaborn plots for daily cases and total cases
         sns.barplot(x='date', y='daily_cases', data=df, ax=ax1,
             palette=sns.color_palette("Set2", 2))
         sns.lineplot(x='date', y='total_cases', data=df, ax=ax2)
-
-        ax1.grid(b=True)
-        ax2.grid(b=True)
-        plt.xticks(rotation = 90, horizontalalignment='right')
-        plt.tight_layout()
-
+        ax1.grid(b=False)
+        ax2.grid(b=False)
+        plt.xticks(rotation = 90, horizontalalignment='center', fontsize= 4)
+        plt.tight_layout(pad = 2.00, h_pad= 2.00)
         image1 = io.StringIO()
         fig1.savefig(image1, format='svg')
         svg_image1 = '<svg' + image1.getvalue().split('<svg')[1]
@@ -76,10 +69,9 @@ def plot_covid():
         # - total_cases (x-axis) and 
         # - smoothed daily_cases (y-axis)
         # as used in: https://www.youtube.com/watch?v=54XLXg4fYsc
-
         fig2 = plt.figure()
+        fig2.suptitle("Smooth daily cases vs total cases from March 6th to May 14th")
         FigureCanvas(fig2)
-
         x_str='total_cases'
         y_str='smooth_daily_cases'
         # using log-log and two minor ticks per decade
@@ -97,7 +89,6 @@ def plot_covid():
         ax.xaxis.set_minor_formatter(FormatStrFormatter("%.0f"))
         ax.yaxis.set_major_formatter(FormatStrFormatter("%.0f"))
         ax.yaxis.set_minor_formatter(FormatStrFormatter("%.0f"))
-
         img2 = io.StringIO()
         fig2.savefig(img2, format='svg')
         svg_image2 = '<svg' + img2.getvalue().split('<svg')[1]
@@ -111,4 +102,3 @@ def plot_covid():
 if __name__ == '__main__':
     app.debug = True
     app.run()
-
